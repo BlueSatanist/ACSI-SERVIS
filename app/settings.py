fb33dfeca99e04d24f76f563a78ce1bd272162e9
BlueSatanist <111049031+BlueSatanist@users.noreply.github.com>
2024-06-07 05:29:40 +0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -89,18 +89,27 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'name',
+#         'USER': 'name',
+#         'PASSWORD': 'name',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'name',
-        'USER': 'name',
-        'PASSWORD': 'name',
-        'HOST': 'localhost',
+        'NAME': 'acsi',
+        'USER': 'acsi',
+        'PASSWORD': 'acsi',
+        'HOST': '91.224.86.176',
         'PORT': '5432',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
