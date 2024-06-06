2a09cd47f8e0e7f0223a5374ba8e50ee287cbb1e
BlueSatanist <111049031+BlueSatanist@users.noreply.github.com>
2024-06-07 06:50:00 +0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -89,17 +89,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'name',
-#         'USER': 'name',
-#         'PASSWORD': 'name',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
